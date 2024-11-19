2c7ed094c95e87d42326ac74495fa2a84a80f530
Retesh Kumar Sharma <rksharma7071@gmail.com>
2024-11-19 22:48:07 +0530

    Remove unused firebase folders

--- dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,9 +0,0 @@
-const { getDataConnect, validateArgs } = require('firebase/data-connect');
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'Fashion-Fusion',
-  location: 'us-central1'
-};
-exports.connectorConfig = connectorConfig;
-
